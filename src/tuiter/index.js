4ff128d031a32478b4d1b27ad00dbe2329764d8b
jarriny7 <jarrin.y@northeastern.edu>
2023-03-27 08:51:05 -0400

    completed 3.1 and 3.2

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -3,8 +3,19 @@ import ExploreComponent from "./explore";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
 
+import {Routes, Route} from "react-router";
+import whoReducer
+    from "./reducers/who-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
+import { configureStore }
+    from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore(
+    {reducer: {who: whoReducer, tuits: tuitsReducer}});
+
 function Tuiter() {
     return (
+        <Provider store={store}>
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                 <NavigationSidebar active="home"/>
@@ -17,6 +28,7 @@ function Tuiter() {
                 <WhoToFollowList/>
             </div>
         </div>
+        </Provider>
     );
 }
 
