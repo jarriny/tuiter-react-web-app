d915b7e3f6df580a739d6ad58a6ba69090800e1a
jarriny7 <jarrin.y@northeastern.edu>
2023-03-27 10:25:38 -0400

    finished A7

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ExploreComponent from "./explore";
+//import ExploreComponent from "./explore";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
 
@@ -10,6 +10,8 @@ import tuitsReducer from "./tuits/tuits-reducer";
 import { configureStore }
     from '@reduxjs/toolkit';
 import {Provider} from "react-redux";
+import TuitsList from "./tuits/tuits-list";
+import HomeComponent from "./home";
 const store = configureStore(
     {reducer: {who: whoReducer, tuits: tuitsReducer}});
 
@@ -22,7 +24,7 @@ function Tuiter() {
             </div>
             <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
-                <ExploreComponent/>
+                <HomeComponent/>
             </div>
             <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>
